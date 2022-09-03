c4f558bae17290b5f28e5034a6d93bcefd886cd6
Masaki Hara <ackie.h.gmai@gmail.com>
2022-09-03 18:53:19 +0900

    Add walk

---
+++ crates/testdata-walk/src/lib.rs
@@ -0,0 +1,133 @@
+use std::collections::HashSet;
+use std::path::{Path, PathBuf, StripPrefixError};
+
+use thiserror::Error as StdError;
+use walkdir::WalkDir;
+
+pub fn walk(spec: &GlobSpec) -> Result<Vec<String>, Error> {
+    walk_dir(spec, Path::new("."))
+}
+
+pub fn walk_dir(spec: &GlobSpec, root: &Path) -> Result<Vec<String>, Error> {
+    let mut stems = HashSet::new();
+    let args = spec
+        .args
+        .iter()
+        .map(|arg| arg.parse())
+        .collect::<Result<Vec<_>, _>>()?;
+    let glob_type = if !args.is_empty() {
+        let glob_type = args[0].glob_type;
+        if !args.iter().all(|arg| arg.glob_type == glob_type) {
+            return Err(Error::MixedGlob);
+        }
+        glob_type
+    } else {
+        GlobType::Recursive
+    };
+    for entry in WalkDir::new(root).sort_by_file_name() {
+        let entry = entry?;
+        let file_name = entry
+            .path()
+            .strip_prefix(root)
+            .map_err(|e| Error::StripPrefix(e, root.to_owned(), entry.path().to_owned()))?;
+        let file_name = file_name
+            .to_str()
+            .ok_or_else(|| Error::InvalidPath(entry.path().to_owned()))?;
+        for arg in &args {
+            if file_name.starts_with(&arg.prefix) && file_name.ends_with(&arg.suffix) {
+                let stem = &file_name[arg.prefix.len()..file_name.len() - arg.suffix.len()];
+                if glob_type == GlobType::Recursive || !stem.contains('/') {
+                    stems.insert(stem.to_owned());
+                }
+            }
+        }
+    }
+    let sorted_stems = {
+        let mut sorted_stems = stems.into_iter().collect::<Vec<_>>();
+        sorted_stems.sort();
+        sorted_stems
+    };
+
+    Ok(sorted_stems)
+}
+
+#[derive(Debug, StdError)]
+pub enum Error {
+    #[error("Error during walk: {0}")]
+    Walkdir(#[from] walkdir::Error),
+    #[error("Cannot compute relative path from {} to {}", .1.display(), .2.display())]
+    StripPrefix(#[source] StripPrefixError, PathBuf, PathBuf),
+    #[error("Got a non-utf8 path: {0:?}")]
+    InvalidPath(PathBuf),
+    #[error("Invalid glob: {0:?}")]
+    InvalidGlob(String),
+    #[error("Different glob types are mixed")]
+    MixedGlob,
+}
+
+#[derive(Debug, Clone)]
+#[non_exhaustive]
+pub struct GlobSpec {
+    pub args: Vec<ArgSpec>,
+}
+
+impl GlobSpec {
+    pub fn new() -> Self {
+        Self { args: Vec::new() }
+    }
+
+    pub fn arg(mut self, arg: ArgSpec) -> Self {
+        self.args.push(arg);
+        self
+    }
+}
+
+#[derive(Debug, Clone)]
+#[non_exhaustive]
+pub struct ArgSpec {
+    pub path: String,
+}
+
+impl ArgSpec {
+    pub fn new(path: &str) -> Self {
+        Self {
+            path: path.to_owned(),
+        }
+    }
+
+    fn parse(&self) -> Result<ParsedArgSpec, Error> {
+        let pos = self
+            .path
+            .find('*')
+            .ok_or_else(|| Error::InvalidGlob(self.path.clone()))?;
+        let (pos2, glob_type) = if self.path[pos..].starts_with("**/*") {
+            (pos + 4, GlobType::Recursive)
+        } else {
+            (pos + 1, GlobType::Single)
+        };
+        let suffix = &self.path[pos2..];
+        if suffix.contains('*') {
+            return Err(Error::InvalidGlob(self.path.clone()));
+        }
+        Ok(ParsedArgSpec {
+            prefix: self.path[..pos].to_owned(),
+            glob_type,
+            suffix: suffix.to_owned(),
+        })
+    }
+}
+
+#[derive(Debug)]
+struct ParsedArgSpec {
+    prefix: String,
+    glob_type: GlobType,
+    suffix: String,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum GlobType {
+    /// `**/*`
+    Recursive,
+    /// `*`
+    Single,
+}
---
+++ crates/testdata-walk/tests/walk_tests.rs
@@ -0,0 +1,102 @@
+use std::path::Path;
+
+use testdata_walk::{walk_dir, ArgSpec, GlobSpec};
+
+#[test]
+fn test_walk_dir() {
+    let stems = walk_dir(
+        &GlobSpec::new()
+            .arg(ArgSpec::new("data/**/*-in.txt"))
+            .arg(ArgSpec::new("data/**/*-out.txt")),
+        Path::new("tests/fixtures/project1"),
+    )
+    .unwrap();
+    assert_eq!(
+        stems,
+        vec![
+            "bar".to_owned(),
+            "baz".to_owned(),
+            "foo".to_owned(),
+            "nested/bar".to_owned(),
+            "nested/baz".to_owned(),
+            "nested/foo".to_owned()
+        ]
+    );
+}
+
+#[test]
+fn test_walk_dir_non_nested() {
+    let stems = walk_dir(
+        &GlobSpec::new()
+            .arg(ArgSpec::new("data/*-in.txt"))
+            .arg(ArgSpec::new("data/*-out.txt")),
+        Path::new("tests/fixtures/project1"),
+    )
+    .unwrap();
+    assert_eq!(
+        stems,
+        vec!["bar".to_owned(), "baz".to_owned(), "foo".to_owned(),]
+    );
+}
+
+#[test]
+fn test_walk_dir_invalid_glob1() {
+    let e = walk_dir(
+        &GlobSpec::new().arg(ArgSpec::new("data/*-*.txt")),
+        Path::new("tests/fixtures/project1"),
+    )
+    .unwrap_err();
+    assert_eq!(e.to_string(), "Invalid glob: \"data/*-*.txt\"");
+}
+
+#[test]
+fn test_walk_dir_invalid_glob2() {
+    let e = walk_dir(
+        &GlobSpec::new().arg(ArgSpec::new("data/in.txt")),
+        Path::new("tests/fixtures/project1"),
+    )
+    .unwrap_err();
+    assert_eq!(e.to_string(), "Invalid glob: \"data/in.txt\"");
+}
+
+#[test]
+fn test_walk_dir_mixed_glob() {
+    let e = walk_dir(
+        &GlobSpec::new()
+            .arg(ArgSpec::new("data/**/*-in.txt"))
+            .arg(ArgSpec::new("data/*-out.txt")),
+        Path::new("tests/fixtures/project1"),
+    )
+    .unwrap_err();
+    assert_eq!(e.to_string(), "Different glob types are mixed");
+}
+
+#[test]
+fn test_walk_dir_no_args() {
+    let stems = walk_dir(&GlobSpec::new(), Path::new("tests/fixtures/project1")).unwrap();
+    assert_eq!(stems, vec![] as Vec<String>);
+}
+
+#[test]
+fn test_glob_spec_clone() {
+    let glob_spec = GlobSpec::new();
+    let _ = glob_spec.clone();
+}
+
+#[test]
+fn test_glob_spec_debug() {
+    let glob_spec = GlobSpec::new();
+    let _ = format!("{:?}", glob_spec);
+}
+
+#[test]
+fn test_arg_spec_clone() {
+    let arg_spec = ArgSpec::new("src/**/*.txt");
+    let _ = arg_spec.clone();
+}
+
+#[test]
+fn test_arg_spec_debug() {
+    let arg_spec = ArgSpec::new("src/**/*.txt");
+    let _ = format!("{:?}", arg_spec);
+}
