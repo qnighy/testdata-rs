94092a9e4e16a6f936e3aa77262338c672adbc78
Masaki Hara <ackie.h.gmai@gmail.com>
2022-09-15 23:43:29 +0900

    Write example test in e2e

--- crates/e2e/tests/test1.rs
+++ crates/e2e/tests/test1.rs
@@ -1,5 +1,12 @@
+#![cfg(feature = "e2e")]
+
+use std::fs;
+use std::path::Path;
+
 #[testdata::testdata(rebuild = "crates/e2e/tests/test1.rs")]
 #[test]
-fn test_foo(#[glob = "tests/fixtures/**/*-in.txt"] input: &std::path::PathBuf) {
-    //
+fn test_foo(#[glob = "tests/fixtures/**/*-in.txt"] input: &Path) {
+    let text = fs::read(input).unwrap();
+    let text = String::from_utf8_lossy(&text).into_owned();
+    assert_eq!(text, "ok\n");
 }
