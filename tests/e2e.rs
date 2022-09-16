9d0875dd46c20f5b432c58bccd874ff2548668e0
Masaki Hara <ackie.h.gmai@gmail.com>
2022-09-16 23:12:56 +0900

    Add e2e test

---
+++ tests/e2e.rs
@@ -0,0 +1,14 @@
+use std::process::Command;
+
+#[test]
+fn test_e2e() {
+    let cmd = Command::new("cargo")
+        .args(&["test", "-p", "e2e", "--features", "e2e"])
+        .output()
+        .unwrap();
+    assert_eq!(cmd.status.code(), Some(101));
+    let stdout = String::from_utf8_lossy(&cmd.stdout);
+    assert!(stdout.contains("test test_foo::foo ... ok"));
+    assert!(stdout.contains("test test_foo::bar ... FAILED"));
+    assert!(stdout.contains("test test_foo::__others ... ok"));
+}
